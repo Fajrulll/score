347f1112c88d3cbfefd4a38177a0e28ce6359ba5
Fajrulll <72296502+Fajrulll@users.noreply.github.com>
2023-10-12 09:28:07 +0700

    Add files via upload
    studi kasus 3,4

---
+++ score/app.py
@@ -0,0 +1,18 @@
+from cs50 import SQL
+# import tools utk website
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+
+app = Flask(__name__)
+
+db = SQL("sqlite:///score.db")
+
+@app.route('/', methods=["GET", "POST"]) #root route
+def index(): #function index
+    if request.method == "POST":
+        name = request.form.get("name")
+        score = request.form.get("score")
+        db.execute("INSERT INTO score (name, score) values(?,?)", name, score)
+        return redirect("/")
+    else:
+        students = db.execute("select * from score")
+        return render_template("index.html", students=students)
