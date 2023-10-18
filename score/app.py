a3ff9dd713a8786e19b6269937e2e9b26095b41e
Fajrulll <72296502+Fajrulll@users.noreply.github.com>
2023-10-18 08:40:13 +0700

    Add files via upload
    studi kasus 5

--- score/app.py
+++ score/app.py
@@ -16,3 +16,19 @@ def index(): #function index
     else:
         students = db.execute("select * from score")
         return render_template("index.html", students=students)
+@app.route("/edit/<id>", methods=["GET", "POST"])
+def edit_data(id):
+    if request.method == "GET":
+        students = db.execute("SELECT * FROM score WHERE id = ?", id)[0]
+        print(students)
+        return render_template("edit.html", students=students)
+    else:
+        students_name = request.form.get("name")
+        students_score = request.form.get("score")
+        db.execute('UPDATE Score set name = ?, score = ? where id = ?', students_name, students_score, id)
+        return redirect("/")
+    
+@app.route("/delete/<id>", methods=["GET"])
+def delete(id):
+    db.execute("delete from score where id = ?",id)
+    return redirect("/")
